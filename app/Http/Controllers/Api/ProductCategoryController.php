dc99886334d2cf809480a0d97b7d06fc7fa38ef1
Jose Roberto <joserobertoteodozio@gmail.com>
2018-10-12 14:39:19 -0300

    primeiro commit

---
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CodeShopping\Http\Controllers\Api;
+
+use CodeShopping\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends Controller
+{
+    //listar relacionamentos
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
