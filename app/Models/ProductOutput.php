f0fbdb122c90c537df7931ae9097efcaf9ff8666
Jose Roberto <joserobertoteodozio@gmail.com>
2018-12-01 19:11:15 -0300

    Atualizacao dia 01.12.2018

--- app/Models/ProductOutput.php
+++ app/Models/ProductOutput.php
@@ -2,11 +2,12 @@
 
 namespace CodeShopping\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductOutput extends Model
 {
-    protected $fillable = ['amount','product_id'];
+    protected $fillable = ['amount', 'product_id'];
 
     public function product(){
         return $this->belongsTo(Product::class);
